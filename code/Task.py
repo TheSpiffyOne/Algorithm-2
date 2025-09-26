632b05d9f6a13241ad1273ad03b7aa7e949be56d
spiffy <86152662+TheSpiffyOne@users.noreply.github.com>
2025-09-26 11:26:15 +0300

    Update Task.py

--- code/Task.py
+++ code/Task.py
@@ -1,1 +1,18 @@
+# Бинарная куча
+import heapq
+min_heap = []                               # минимальная куча
+
+# Операции:
+heapq.heappush(min_heap, value)             # добавление
+heapq.heappop(min_heap)                     # удаление вершины
+min_heap[0]                                 # доступ к вершине
+
+# Хеш таблица
+hash_table = {}                             # создание словаря
+
+# Операции:
+hash_table[key] = value                     # вставка/обновление
+hash_table.get(key)                         # поиск
+del hash_table[key]                         # удаление
+len(hash_table)                             # размер
 
