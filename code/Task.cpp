685fce35350f6d6285c8d685ec14ad3e6c3bcc5b
spiffy <86152662+TheSpiffyOne@users.noreply.github.com>
2025-09-26 11:23:24 +0300

    Update Task.cpp

--- code/Task.cpp
+++ code/Task.cpp
@@ -1,1 +1,20 @@
+// Бинарная куча:
+#include <queue>
+std::priority_queue<int> maxHeap;           // максимальная куча
+std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap; // минимальная куча
+
+// Операции:
+maxHeap.push(value);                        // добавление элемента
+maxHeap.pop();                             // удаление вершины
+maxHeap.top();                             // доступ к вершине
+
+//Хеш таблица
+#include <unordered_map>
+std::unordered_map<KeyType, ValueType> hashTable;
+
+// Операции:
+hashTable[key] = value;                     // вставка/обновление
+hashTable.find(key);                       // поиск
+hashTable.erase(key);                      // удаление
+hashTable.size();                          // размер
 
