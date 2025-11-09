ce943e9a23eccf8a1d6c11ed79b0880ae96b8f2a
spiffy <86152662+TheSpiffyOne@users.noreply.github.com>
2025-11-09 19:45:43 +0300

    Update java.java

--- rabota2/java.java
+++ rabota2/java.java
@@ -1,1 +1,12 @@
-
+public class PowerCalculator {
+    public static double power(double x, int n) {
+        if (n == 0) return 1;
+        if (n < 0) return 1 / power(x, -n);
+        return x * power(x, n - 1);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("2^5 = " + power(2, 5));
+        System.out.println("3^-2 = " + power(3, -2));
+    }
+}
