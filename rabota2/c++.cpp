06f7c10e40fae60745b8d7d333371c0b848fde66
spiffy <86152662+TheSpiffyOne@users.noreply.github.com>
2025-11-09 19:45:27 +0300

    Update c++.cpp

--- rabota2/c++.cpp
+++ rabota2/c++.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+double power(double x, int n) {
+    if (n == 0) return 1;
+    if (n < 0) return 1 / power(x, -n);
+    return x * power(x, n - 1);
+}
+
+int main() {
+    cout << "2^5 = " << power(2, 5) << endl;
+    cout << "3^-2 = " << power(3, -2) << endl;
+    return 0;
+}
